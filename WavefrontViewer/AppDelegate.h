7ef3bfb8de5ffe0d7a1069bcf8aff6a52db60249
Marius <marius@192-168-0-102.rdsnet.ro>
2017-07-17 16:28:05 +0300

    Initial version

---
+++ WavefrontViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavefrontViewer
+//
+//  Created by Marius Sincovici on 17/07/2017.
+//  Copyright © 2017 Marius Sincovici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WavefrontViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WavefrontViewer
+//
+//  Created by Marius Sincovici on 17/07/2017.
+//  Copyright © 2017 Marius Sincovici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
